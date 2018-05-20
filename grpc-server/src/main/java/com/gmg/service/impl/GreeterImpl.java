43156e4450f2613f26bc0ca3a8d0f3d4e8878462
gmg <774131720@qq.com>
2018-05-20 18:03:46 +0800

    Initial commit

---
+++ grpc-server/src/main/java/com/gmg/service/impl/GreeterImpl.java
@@ -0,0 +1,19 @@
+package com.gmg.service.impl;
+
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+/**
+ * Created by gmg on 2018/5/20.
+ */
+public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+@Override
+public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+        }
+}
